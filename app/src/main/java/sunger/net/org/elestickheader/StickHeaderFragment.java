4e2956a755a9e7dd78055def9c21436adcaacf7c
sunger <kcjsunger@gamil.com>
2017-10-17 20:38:25 +0800

    update

--- app/src/main/java/sunger/net/org/elestickheader/StickHeaderFragment.java
+++ app/src/main/java/sunger/net/org/elestickheader/StickHeaderFragment.java
@@ -1,5 +1,7 @@
 package sunger.net.org.elestickheader;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +13,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +27,10 @@ import java.util.List;
 
 public class StickHeaderFragment extends Fragment {
     private RecyclerView recyclerView;
+    private RecyclerView recyclerViewFilter;
     private View layoutStickHeader;
+    private View layoutFilter;
+    private ExpandableLayout expandableLayout;
     private CommonAdapter commonAdapter;
     private LinearLayoutManager linearLayoutManager;
     private int top = -1;
@@ -38,8 +47,12 @@ public class StickHeaderFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerViewFilter = (RecyclerView) view.findViewById(R.id.recyclerView_filter);
         layoutStickHeader = view.findViewById(R.id.layout_stick_header);
+        layoutFilter = view.findViewById(R.id.layout_filter);
+        expandableLayout = (ExpandableLayout) view.findViewById(R.id.expandableLayout);
         linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerViewFilter.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setLayoutManager(linearLayoutManager);
         commonAdapter = new CommonAdapter(R.layout.item_common_text);
         List<String> data = new ArrayList<>();
@@ -53,16 +66,42 @@ public class StickHeaderFragment extends Fragment {
         View header2 = LayoutInflater.from(getContext()).inflate(R.layout.view_header_banner2, recyclerView, false);
         commonAdapter.addHeaderView(header2);
 
-        final View header3 = LayoutInflater.from(getContext()).inflate(R.layout.view_stick_header, recyclerView, false);
+        final View header3 = LayoutInflater.from(getContext()).inflate(R.layout.view_header3, recyclerView, false);
         initStickView(header3);
         commonAdapter.addHeaderView(header3);
         header3.setBackgroundColor(Color.BLUE);
 
         commonAdapter.addData(data);
         recyclerView.setAdapter(commonAdapter);
+        layoutFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expandableLayout.collapse();
+            }
+        });
+        expandableLayout.setOnExpansionUpdateListener(new ExpandableLayout.OnExpansionUpdateListener() {
+            @Override
+            public void onExpansionUpdate(float expansionFraction, int state) {
+                if (state == 0) {
+                    layoutFilter.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        List<String> dataFilter = new ArrayList<>();
+        dataFilter.add("综合排序");
+        dataFilter.add("销量最高");
+        dataFilter.add("起送价最低");
+        dataFilter.add("配送最快");
+        dataFilter.add("配送费最低");
+        dataFilter.add("人均从低到高");
+        dataFilter.add("人均从高到低");
+
+        CommonAdapter filterAdapter = new CommonAdapter(R.layout.item_filter);
+        filterAdapter.addData(dataFilter);
+        recyclerViewFilter.setAdapter(filterAdapter);
 
     }
-
 
 
     private void initStickView(final View view) {
@@ -70,12 +109,7 @@ public class StickHeaderFragment extends Fragment {
             @Override
             public void onGlobalLayout() {
                 top = view.getTop();
-                view.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        recyclerView.smoothScrollBy(0, view.getTop() - mScrollY);
-                    }
-                });
+                addHeaderFilterClickListener(view);
                 addScrollListener();
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                     view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
@@ -84,6 +118,29 @@ public class StickHeaderFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void addHeaderFilterClickListener(final View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerView.smoothScrollBy(0, view.getTop() - mScrollY);
+                showFilter();
+            }
+        });
+    }
+
+
+    private void showFilter() {
+        layoutFilter.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                layoutFilter.setVisibility(View.VISIBLE);
+                expandableLayout.expand();
+            }
+        }, 300);
+
+
     }
 
     private int mScrollY = 0;
